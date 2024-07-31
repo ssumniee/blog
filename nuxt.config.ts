d2713bcd2a497a34bba977463a5bf30d415cfef9
Leezy <leezy@un7qi3.co>
2024-07-31 12:20:12 +0900

    feat: nuxt config 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import {fileURLToPath} from 'url';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'ssumniee 블로그',
+    },
+  },
+  devtools: {enabled: true},
+  srcDir: 'src/',
+  ssr: true,
+  modules: ['@vueuse/nuxt'],
+  alias: {
+    '@': fileURLToPath(new URL('./src/', import.meta.url)),
+    '~': fileURLToPath(new URL('./', import.meta.url)),
+  },
+  compatibilityDate: '2024-07-31',
+});
